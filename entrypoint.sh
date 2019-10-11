a0f3873078dc7ddf1d5affc74905748ecb0991dd
Stepan Henek <stepan@angelcam.com>
2019-10-11 22:42:34 +0200

    fixup! fixup! fixup! fixup! fixup! using ssh_key for private repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,17 @@ mkdir -p ~/.ssh/
 echo "${INPUT_SSH_KEY}" > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/id_rsa
+
 ssh-keygen -R github.com
+
 echo "Host github.com" > ~/.ssh/config
 echo "    StrictHostKeyChecking no" >> ~/.ssh/config
 
+echo "[net]" > /root/.cargo/config
+echo "git-fetch-with-cli = true" >> /root/.cargo/config
+
 
 set -ex
-
-eval `ssh-agent -s`
-ssh-add
 
 wc -l ~/.ssh/id_rsa
 git clone git@github.com:angelcam/rust-ac-http-server.git /tmp/testik
