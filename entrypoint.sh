f745676154d74c31a124b1064ec4f24cffff7863
Stepan Henek <stepan@angelcam.com>
2019-10-11 14:42:43 +0200

    using ssh_key for private repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@
 # this will cause that the new toolchains are not visible
 export HOME=/root/
 
+mkdir -p ~/.ssh/
+echo "${INPUTS_SSH_KEY}" > ~/.ssh/id_rsa
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+
 sleep 1
 
 set -ex
