177c00401049053d296c948fff158d62b2e03982
Stepan Henek <stepan@angelcam.com>
2019-10-11 23:01:03 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! using ssh_key for private repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,24 @@
 # this will cause that the new toolchains are not visible
 export HOME=/root/
 
+# Store private ssh key passed by env variable
 mkdir -p ~/.ssh/
 echo "${INPUT_SSH_KEY}" > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/id_rsa
 
-ssh-keygen -R github.com
 
+#TODO move these lines to base docker image
+ssh-keygen -R github.com
 echo "Host github.com" > ~/.ssh/config
 echo "    StrictHostKeyChecking no" >> ~/.ssh/config
-
 echo "[net]" > /root/.cargo/config
 echo "git-fetch-with-cli = true" >> /root/.cargo/config
 
 
 set -ex
 
+#TODO remove these two lines
 wc -l ~/.ssh/id_rsa
 git clone git@github.com:angelcam/rust-ac-http-server.git /tmp/testik
 
