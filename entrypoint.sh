66e3532953808b690820b4b2488a9cff6c7292e0
Stepan Henek <stepan@henek.name>
2019-10-15 11:02:20 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! using ssh_key for private repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,8 @@
 export HOME=/root/
 
 # Store private ssh key passed by env variable
-mkdir -p ~/.ssh/
 echo "${INPUT_SSH_KEY}" > ~/.ssh/id_rsa
-chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/id_rsa
-
-
-#TODO move these lines to base docker image
-ssh-keygen -R github.com
-echo "Host github.com" > ~/.ssh/config
-echo "    StrictHostKeyChecking no" >> ~/.ssh/config
-echo "[net]" > /root/.cargo/config
-echo "git-fetch-with-cli = true" >> /root/.cargo/config
-
 
 set -ex
 
